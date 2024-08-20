19bd354aaa2d7689a82af028a47051017abfcb56
dunpai <dunpai@i9i8.com>
2024-08-20 10:15:24 +0800

    init

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String[] abc){
+        int x = 100;
+        System.out.println(x);
+        x = 200;
+        System.out.println(x);
+    }
+}
