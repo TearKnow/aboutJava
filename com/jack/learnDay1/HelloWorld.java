97e3c36feed6aa1a7d73d582d2384192f868d333
Jack <904916790@qq.com>
2024-08-21 22:42:02 +0800

    add git ignore

--- com/jack/learnDay1/HelloWorld.java
+++ com/jack/learnDay1/HelloWorld.java
@@ -1,5 +1,7 @@
+package com.jack.learnDay1;
+
 public class HelloWorld{
-    public static void main(String[] abc){
+    public static void main(String[] args){
         int x = 100;
         System.out.println(x);
         x = 200;
