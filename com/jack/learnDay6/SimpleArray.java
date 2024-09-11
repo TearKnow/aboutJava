e1411320ba42aa265befb7a194b4f48ffb190dfb
Jack <904916790@qq.com>
2024-09-11 21:56:59 +0800

    add my array

---
+++ com/jack/learnDay6/SimpleArray.java
@@ -0,0 +1,22 @@
+package com.jack.learnDay6;
+
+public class SimpleArray {
+    public static void main(String[] args) {
+
+        int[] bb = new int[5];
+        bb[0] = 1;
+        for (int i = 0; i < 5; i++) {
+            System.out.print(bb[i]);
+        }
+
+
+
+
+        int[] cc = {1,2,3,4};
+        for (int i = 0; i < 4; i++) {
+            System.out.print(cc[i]);
+        }
+
+
+    }
+}
