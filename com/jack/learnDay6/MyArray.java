e649773dfbc1dfc4c6eaae6c165fc57a1e049f87
Jack <904916790@qq.com>
2024-09-22 22:35:50 +0800

    fix

--- com/jack/learnDay6/MyArray.java
+++ com/jack/learnDay6/MyArray.java
@@ -1,11 +1,17 @@
 package com.jack.learnDay6;
 
+import java.util.Arrays;
+
 public class MyArray {
     public static void main(String[] args) {
         String[] myarr = {"张三","李四"};
         System.out.println(myarr.length);
 
 
+        Integer[] abc = new Integer[]{1,2,3,4};
+        System.out.println(Arrays.toString(abc));
 
+        int[] bcd = new int[]{5,6,7,8};
+        System.out.println(Arrays.toString(bcd));
     }
 }
